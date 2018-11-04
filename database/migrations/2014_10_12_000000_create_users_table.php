41ba837045abf4cfc94a74784ad1f3cdab051b16
oyghan <742163033@qq.com>
2018-11-04 16:48:55 +0800

    user migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,14 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('avatar')->default('images/avatar.jpg');
+            $table->boolean('is_admin')->default(false); //是否为管理员
+            $table->string('activation_token', 80)->nullable(); //激活token
+            $table->boolean('is_activated')->default(false); //是否已激活
+            $table->integer('experience')->nullable()->comment('积分');
+            $table->string('api_token', 80)->nullable();
+            $table->enum('sex', ['male', 'female']);
+            $table->timestamp('last_login_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
