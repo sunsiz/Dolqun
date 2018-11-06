f2e0ea29532a3fef714d32771d4e85392f5d64b2
oyghan <742163033@qq.com>
2018-11-06 18:21:47 +0800

    user avatar

--- resources/views/layouts/global.blade.php
+++ resources/views/layouts/global.blade.php
@@ -11,63 +11,76 @@
     <link href="{{ asset('css/bootstrap-3.3.7/css/bootstrap.min.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/bootstrap-3.3.7/css/bootstrap-rtl.css') }}">
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
-
+    <script>
+        @if(Auth::check())
+            window.LEARNFANS = {
+            name:"{{Auth::user()->name}}",
+            avatar:"{{Auth::user()->avatar}}",
+            token:"{{ csrf_token() }}"
+        }
+        @endif
+    </script>
 </head>
 <body>
+<div id="app">
+    <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                        data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">
+                    <img src="{{ asset('./images/logo-icon.png') }}" alt="">
+                </a>
+            </div>
 
-<nav class="navbar navbar-default navbar-fixed-top">
-    <div class="container">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
-                    data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#">
-                <img src="{{ asset('./images/logo-icon.png') }}" alt="">
-            </a>
-        </div>
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-
-            <ul class="nav navbar-nav">
-                <li class="active"><a href="{{ route('home') }}"> باشبەت<span class="sr-only">(current)</span></a></li>
-                <li><a href="#">قامۇس</a></li>
-                <li><a href="#">فىلغەت</a></li>
-
-            </ul>
-
-            <ul class="nav navbar-nav navbar-right">
-                @auth()
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"  data-hover="dropdown">
-                            {{ Auth::user()->name }}
-                            <img src="{{ asset(Auth::user()->avatar) }}" alt="" class="img-circle" height="18">
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a href="{{ route('user.show', Auth::user()->id) }}"><i class="fa fa-user"></i> ئەزا مەركىزى</a></li>
-                            {{--<li><a href="{{ route('user.edit', Auth::user()->id) }}"><i class="fa fa-edit"></i> ئەزا تەڭشىكى</a></li>--}}
-                            <li><a href="{{ route('logout') }}"><i class="fa fa-sign-out"></i> بېخەتەر چىكىنىش</a></li>
-                        </ul>
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="{{ route('home') }}"> باشبەت<span class="sr-only">(current)</span></a>
                     </li>
-                @else
-                    <li><a href="{{ route('login') }}">كىرىش</a></li>
-                    <li><a href="{{ route('register') }}">تېزىملىتىش</a></li>
-                @endauth
-            </ul>
-        </div><!-- /.navbar-collapse -->
-    </div><!-- /.container-fluid -->
-</nav>
-<div class="container">
-    <div class="row" style="padding-top:60px;">
-        <div class="col-md-12">
-            @include('layouts.messages')
-        </div>
-        <div class="col-md-12">
-            @yield('content')
+                    <li><a href="#">قامۇس</a></li>
+                    <li><a href="#">فىلغەت</a></li>
+
+                </ul>
+
+                <ul class="nav navbar-nav navbar-right">
+                    @auth()
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
+                               aria-haspopup="true" aria-expanded="false" data-hover="dropdown">
+                                {{ Auth::user()->name }}
+                                <img src="{{ asset(Auth::user()->avatar) }}" alt="" class="img-circle" height="18">
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a href="{{ route('user.show', Auth::user()->id) }}"><i class="fa fa-user"></i> ئەزا
+                                        مەركىزى</a></li>
+                                {{--<li><a href="{{ route('user.edit', Auth::user()->id) }}"><i class="fa fa-edit"></i> ئەزا تەڭشىكى</a></li>--}}
+                                <li><a href="{{ route('logout') }}"><i class="fa fa-sign-out"></i> بېخەتەر چىكىنىش</a>
+                                </li>
+                            </ul>
+                        </li>
+                    @else
+                        <li><a href="{{ route('login') }}">كىرىش</a></li>
+                        <li><a href="{{ route('register') }}">تېزىملىتىش</a></li>
+                    @endauth
+                </ul>
+            </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+    </nav>
+    <div class="container">
+        <div class="row" style="padding-top:60px;">
+            <div class="col-md-12">
+                @include('layouts.messages')
+            </div>
+            <div class="col-md-12">
+                @yield('content')
+            </div>
         </div>
     </div>
 </div>
