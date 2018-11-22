fa987535e27684292e26def257e6e99b530e290c
oyghan <742163033@qq.com>
2018-11-22 10:38:20 +0800

    photos auth

--- app/Http/Controllers/Home/PhotosController.php
+++ app/Http/Controllers/Home/PhotosController.php
@@ -10,6 +10,13 @@ use App\Http\Requests\CreatePhotoRequest;
 
 class PhotosController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth', [
+            'except' => ['index', 'show']
+        ]);
+    }
+
     public function index()
     {
         $photos = Photo::orderBy('id', 'DESC')->paginate(10);
