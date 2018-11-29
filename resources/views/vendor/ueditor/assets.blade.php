0331993f11727d50dd11da9c362e94fa39e9bb3e
oyghan <742163033@qq.com>
2018-11-29 15:04:14 +0800

    Ueditor/uighurInput

--- resources/views/vendor/ueditor/assets.blade.php
+++ resources/views/vendor/ueditor/assets.blade.php
@@ -1,7 +1,8 @@
 <!-- 配置文件 -->
-<script type="text/javascript" src="{{ asset('vendor/ueditor/ueditor.config.js') }}"></script>
+<script type="text/javascript" src="{{ asset('vendor/ueditor/ueditor.configs.js') }}"></script>
 <!-- 编辑器源码文件 -->
 <script type="text/javascript" src="{{ asset('vendor/ueditor/ueditor.all.js') }}"></script>
+<script type="text/javascript" charset="utf-8" src="{{asset('vendor/ueditor/NUIedit.js')}}"> </script>
 <script>
     window.UEDITOR_CONFIG.serverUrl = '{{ config('ueditor.route.name') }}'
 </script>
