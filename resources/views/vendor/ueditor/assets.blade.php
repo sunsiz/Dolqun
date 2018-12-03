0571657f7a2e31b137febac7c2be9586843e3298
oyghan <742163033@qq.com>
2018-12-03 22:54:18 +0800

    ueditor font/background color config

--- resources/views/vendor/ueditor/assets.blade.php
+++ resources/views/vendor/ueditor/assets.blade.php
@@ -1,5 +1,5 @@
 <!-- 配置文件 -->
-<script type="text/javascript" src="{{ asset('vendor/ueditor/ueditor.configs.lf.js') }}"></script>
+<script type="text/javascript" src="{{ asset('vendor/ueditor/ueditor.configs.dl.js') }}"></script>
 <!-- 编辑器源码文件 -->
 <script type="text/javascript" src="{{ asset('vendor/ueditor/ueditor.all.js') }}"></script>
 <script type="text/javascript" charset="utf-8" src="{{asset('vendor/ueditor/NUIedit.js')}}"> </script>
