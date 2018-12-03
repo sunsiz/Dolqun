abef5444a20af57a0c836d119ab7792b4d475ac4
oyghan <742163033@qq.com>
2018-12-03 23:17:58 +0800

    show page user-panel

--- resources/views/users/user-card.blade.php
+++ resources/views/users/user-card.blade.php
@@ -1,19 +1,16 @@
-<div class="bg-white br3" style="overflow: hidden;padding-top: 25px;margin-bottom: 20px">
-    <div class="col-md-12">
-        <div class="lf-ucenter-avatar" style="text-align: center;">
-            <img src="{{ asset($user->avatar) }}" alt="{{ $user->name }}"  class="img-circle" style="width: 80px;display: block;margin:0 auto 25px auto">
-            <h3>{{ $user->name }}</h3>
-            <p>
-                @if($user->is_admin)
-                    <span class="text-pink">بېكەت باشلىقى</span>
-                @else
-                    <span class="text-muted">يېڭى ئەزا</span>
-                @endif
-                {{--<span class="text-muted">|</span>--}}
-                {{--<span class="text-pink">@پاراڭلىشىش</span>--}}
-                <br>
-                <br>
-            </p>
+<div class="bg-white br3" style="overflow: hidden;padding: 15px 0;margin-bottom: 20px;line-height: 20px;">
+    <div class="row" style="text-align: center">
+        <div class="col-md-12 col-sm-4 col-xs-4">
+            <img src="{{ asset($user->avatar) }}" alt="{{ $user->name }}"  class="img-circle" style="height: 45px">
+        </div>
+        <div class="col-md-12 col-sm-3 col-xs-3" style="padding:10px 0;">
+            <span style="font-size: 16px;">
+                {{ $user->name }}
+            </span>
+        </div>
+        <div class="col-md-12 col-sm-5 col-xs-5">
+            <button class="btn btn-sm btn-outline-pink" style="padding:6px 18px;border-radius: 15px;margin-top: 6px;">ئاپتور</button>
+            <br>
         </div>
     </div>
 </div>
